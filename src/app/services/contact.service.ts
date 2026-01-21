076f2a67e8fca81e83dc6caeb492cfddb152730a
elieEscal67 <elie.ferradji@escal.fr>
2026-01-21 17:00:26 +0100

    add some method in service

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -6,5 +6,18 @@ import { Contact } from '../../models/contact-model';
 })
 export class ContactService {
 
-  
+   private contacts: Contact[] = [];
+
+   getContacts() : Contact[] {
+    return this.contacts;
+   }
+
+   addContact(contact : Contact){
+    return this.contacts.push(contact)
+
+   }
+
+   deleteContact(id : number)  {
+    return this.contacts = this.contacts.filter(c => c.id !== c.id);
+}
 }
