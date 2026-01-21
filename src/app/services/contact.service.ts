6a4bd633b94fbdee0b29c8ee50abd379ee20e139
elieEscal67 <elie.ferradji@escal.fr>
2026-01-21 16:17:07 +0100

    create contact-fom and contact-list component and model and service

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../../models/contact-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  
+}
